3391a565f60bb8f25fbcb3dc0228e78e604c906e
anusha-s18 <anushasrivastav18@gmail.com>
2024-11-19 13:58:04 +0530

    First

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDOzdQx0_UzrNaOh3DV9Xa0U8m5JDXH3c",
+  authDomain: "green-commerce-ae779.firebaseapp.com",
+  projectId: "green-commerce-ae779",
+  storageBucket: "green-commerce-ae779.firebasestorage.app",
+  messagingSenderId: "933127450101",
+  appId: "1:933127450101:web:68649a6c604150682144bc"
+};
